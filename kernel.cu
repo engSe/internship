afa25239df40a4fd0ad95f09f1aaa87971b31282
engSe <snu11ee@gmail.com>
2015-07-09 13:23:32 +0900

    Update kernel.cu
    일단 전체적으로

--- kernel.cu
+++ kernel.cu
@@ -11,68 +11,73 @@
 #include <cusparse.h>
 #include <time.h>
 
-template <typename> vector_insert(int n,ifstream file,T a);
-void vector_output(int n, double * vectors,string filename);
+
 
 
 using namespace std;
 
+template <typename T> 
+T* vector_insert(int n, string filename, T a);
+void vector_output(int n, double * vectors, string filename);
+void solve(int nnz, int  n, double tol, double* dVal,   int * dCol,   int * dRow, double* dbvec, double* dx);
+
 int main(){
 
-// initial val 
-cout<<"n and nnz"<<endl;
+	int n, nnz;
+	// initial val 
+	cout << "n and nnz" << endl;
 
-cin<<n<<nnz;
+	cin >> n >> nnz;
 
-            //csr 
-double *result=new double[n];
-unsigned int *rowPtr = new unsigned int[n+1];
-unsigned int *colidx = new unsigned int[nnz];
-double *csrval = new double[nnz]; 
-double *bvec =new double[n];
+	//csr 
+	double *result = new double[n];
+	  int *rowPtr = new   int[n + 1];
+	  int *colidx = new   int[nnz];
+	double *csrval = new double[nnz];
+	double *bvec = new double[n];
 
 
-// unsign int range
-if (nnz> 4294967295)
-return 1;
+	// unsign int range
+	if (nnz> 4294967295)
+		return 1;
 
-if(n>4294967295)
-cout<<"warn"<<endl;
+	if (n>4294967295)
+		cout << "warn" << endl;
 
-//input :::::::::input은 ascii code로 저장되어야 한다.
-
-ifstream finb,finROW,finCOL,finVAL;
-
-string finb="sysb.mat";
-string finROW = "rowPtr.mat";
-string finCOL = "colidx.mat";
-string finVAL = "val.mat";
-
-double dou=1.0;
-unsigned int uint=1;
-
-bvec=vector_insert(n,finb,dou);
-rowPtr=vector_insert(n+1,finROW,uint);
-colidx=vector_insert(nnz,finCOL,uint);
-csrval=vector_insert(nnz,finVAL,dou);
+	//input :::::::::input은 ascii code로 저장되어야 한다.
 
 
 
-//cuda alloc
+	string finb = "sysb.mat";
+	string finROW = "rowPtr.mat";
+	string finCOL = "colidx.mat";
+	string finVAL = "val.mat";
 
-  unsigned int* dCol, *dRow;
-	double* dVal,dbvec,dx;
-  cudaError_t error;
+	double dou = 1.0;
+	  int uint = 1;
 
-	cudatMalloc((void**)&dx,sizeof(double)*n);
-	cudatMalloc((void**)&dbvec,sizeof(double)*n);
+	bvec = vector_insert(n, finb, dou);
+	rowPtr = vector_insert(n + 1, finROW, uint);
+	colidx = vector_insert(nnz, finCOL, uint);
+	csrval = vector_insert(nnz, finVAL, dou);
+
+
+
+	//cuda alloc
+
+	  int* dCol, *dRow;
+	double* dVal, *dbvec, *dx;
+	cudaError_t error;
+
+	cudaMalloc((void**)&dx, sizeof(double)*n);
+	cudaMalloc((void**)&dbvec, sizeof(double)*n);
 	cudaMalloc((void**)&dCol, sizeof(int)*nnz);
 	cudaMalloc((void**)&dRow, sizeof(int)*(n + 1));
 	cudaMalloc((void**)&dVal, sizeof(double)*nnz);
-	
-	
 
-	cudaMemcpy(dbvec,bvec,sizeof(double)*n,cudaMemcpyHostToDevice);
+
+
+	cudaMemcpy(dbvec, bvec, sizeof(double)*n, cudaMemcpyHostToDevice);
 	cudaMemcpy(dCol, colidx, sizeof(int)*nnz, cudaMemcpyHostToDevice);
 	cudaMemcpy(dRow, rowPtr, sizeof(int)*(n + 1), cudaMemcpyHostToDevice);
 	cudaMemcpy(dVal, csrval, sizeof(double)*nnz, cudaMemcpyHostToDevice);
@@ -89,48 +94,48 @@ csrval=vector_insert(nnz,finVAL,dou);
 	std::cout << "status cusparseCreate: " << cusparse_status << std::endl;
 	cusolver_status = cusolverSpCreate(&cusolver_handle);
 	std::cout << "status cusolverSpCreate: " << cusolver_status << std::endl;
-// solve
-cudaDeviceSynchronize();
+	// solve
+	cudaDeviceSynchronize();
 
-	tol=1e-6;
+	double tol = 1e-6;
 	solve(nnz, n, tol, dVal, dCol, dRow, dbvec, dx);
 
 	error = cudaGetLastError();
 	std::cout << "Error status after solve(): " << error << std::endl;
 
-cudaDeviceSynchronize();
+	cudaDeviceSynchronize();
 
 
 
-// return
+	// return
 
 
-cudaMemcpy(result,dx,n*sizeof(double),cudaMemcpyDeviceToHost);
+	cudaMemcpy(result, dx, n*sizeof(double), cudaMemcpyDeviceToHost);
 
 
-// OUTPUT
+	// OUTPUT
 
-vector_output(n,result,"x_val.txt");
+	vector_output(n, result, "x_val.txt");
 
-//free
+	//free
 
-cudaFree(dCOL);
-cudaFree(dROW);
-cudaFree(dx);
-cudaFree(dbvec);
-cudaFree(dVal);
+	cudaFree(dCol);
+	cudaFree(dRow);
+	cudaFree(dx);
+	cudaFree(dbvec);
+	cudaFree(dVal);
 
 
-return 0;
+	return 0;
 }
 
 
 //cuda function
 
-void solve(int nnz,int  n,double tol,double* dVal,double* dCol,double * dRow,double* dbvec,double* dx){
-  
-  
-  // --- prepare solving and copy to GPU:
+void solve(int nnz, int  n, double tol, double* dVal,   int* dCol,   int * dRow, double* dbvec, double* dx){
+
+
+	// --- prepare solving and copy to GPU:
 	int reorder = 0;
 	int singularity = 0;
 
@@ -150,38 +155,39 @@ void solve(int nnz,int  n,double tol,double* dVal,double* dCol,double * dRow,dou
 }
 
 /// 입출력
-void vector_output(int n, double * vectors,string filename){
-	
+void vector_output(int n, double * vectors, string filename){
+
 	ofstream file(filename);
-	int i=0;
-	while(i<n)
+	int i = 0;
+	while (i<n)
 	{
-		file<<vectors[i];
+		file << vectors[i];
 		i++;
 	}
+	file.close();
 }
 template <typename T>
-T* vector_insert(int n,string filename,T a){
+T* vector_insert(int n, string filename, T a){
 
 
-ifstream file;
-file.open(filename);
+	ifstream file;
+	file.open(filename);
 
-if(!file)
-{
-  cout<<"file input error"<<endl;
-  return 1;
-}
+	if (!file)
+	{
+		cout << "file input error" << endl;
+		return 1;
+	}
 
-T *vectors=new T[n];
-int i=0;
-while(i<n &&file   ){
-file<<vectors[i];
+	T *vectors = new T[n];
+	int i = 0;
+	while (i<n &&file){
+		file << vectors[i];
 
-i++;
+		i++;
 
-}
+	}
 
-fclose(file);
-return vectors;
+	file.close();
+	return vectors;
 }
